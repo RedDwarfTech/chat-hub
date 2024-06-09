23c5b42814e5aefac8b19e7e3b49dbb6214c25e8
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-06-09 17:35:27 +0800

    feat: add create stream completion function

--- src/service/chat/azure_chat_service.rs
+++ src/service/chat/azure_chat_service.rs
@@ -1,14 +1,20 @@
-use async_openai::types::{ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs};
+use crate::model::req::chat::ask_req::AskReq;
+use async_openai::types::{
+    ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs,
+    CreateChatCompletionResponse, CreateCompletionRequestArgs,
+};
 use async_openai::{config::AzureConfig, types::ChatCompletionRequestSystemMessageArgs, Client};
+use futures::StreamExt;
 use log::error;
 use rust_wheel::common::util::net::sse_message::SSEMessage;
 use std::env;
 use std::error::Error;
 use tokio::sync::mpsc::UnboundedSender;
 
-use crate::model::req::chat::ask_req::AskReq;
-
-pub async fn azure_chat(tx: UnboundedSender<SSEMessage<String>>, req: &AskReq) -> Result<String, Box<dyn Error>> {
+pub async fn azure_chat(
+    tx: UnboundedSender<SSEMessage<String>>,
+    req: &AskReq,
+) -> Result<String, Box<dyn Error>> {
     let azure_chat_api_base =
         env::var("AZURE_CHAT_API_BASE").expect("AZURE_CHAT_API_BASE must be set");
     let deployment_id = env::var("DEPLOYMENT_ID").expect("DEPLOYMENT_ID must be set");
@@ -19,18 +25,17 @@ pub async fn azure_chat(tx: UnboundedSender<SSEMessage<String>>, req: &AskReq) -
         .with_deployment_id(deployment_id)
         .with_api_key(api_key);
     let client = Client::with_config(config);
-    return chat_completion(&client, tx, req).await;
+    return chat_completion_stream(&client, tx, req).await;
 }
 
-async fn chat_completion(
+async fn _chat_completion(
     client: &Client<AzureConfig>,
     tx: UnboundedSender<SSEMessage<String>>,
-    req: &AskReq
+    req: &AskReq,
 ) -> Result<String, Box<dyn Error>> {
     let request = CreateChatCompletionRequestArgs::default()
         .max_tokens(512u16)
         .model("gpt-3.5-turbo")
-        .stream(true)
         .messages([
             ChatCompletionRequestSystemMessageArgs::default()
                 .content("You are a helpful assistant.")
@@ -43,13 +48,44 @@ async fn chat_completion(
         ])
         .build()?;
 
-    let response = client.chat().create(request).await?;
+    let response: CreateChatCompletionResponse = client.chat().create(request).await?;
     let msg_string = serde_json::to_string(&response);
     if let Err(e) = msg_string {
         error!("serial json failed,{}", e);
         return Ok("".to_owned());
     }
     do_msg_send_sync(&msg_string.unwrap(), &tx, "chat");
+    Ok("".to_owned())
+}
+
+async fn chat_completion_stream(
+    client: &Client<AzureConfig>,
+    tx: UnboundedSender<SSEMessage<String>>,
+    req: &AskReq,
+) -> Result<String, Box<dyn Error>> {
+    let request = CreateCompletionRequestArgs::default()
+        .model("gpt-3.5-turbo-instruct")
+        .n(1)
+        .prompt(req.prompt.as_str())
+        .stream(true)
+        .max_tokens(1024_u16)
+        .build()?;
+
+    let mut stream = client.completions().create_stream(request).await?;
+
+    while let Some(response) = stream.next().await {
+        match response {
+            Ok(ccr) => {
+                let msg_string = serde_json::to_string(&ccr);
+                if let Err(e) = msg_string {
+                    error!("serial json failed,{}", e);
+                    return Ok("".to_owned());
+                }
+                do_msg_send_sync(&msg_string.unwrap(), &tx, "chat");
+            }
+            Err(e) => error!("create stream facing issue, {}", e),
+        }
+    }
     Ok("".to_owned())
 }
 
