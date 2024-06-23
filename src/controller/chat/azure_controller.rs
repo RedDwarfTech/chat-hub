d42928b06c308d7d8470a5de17864b0991de1ef7
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-06-23 22:17:45 +0800

    feat: add vip expire msg

--- src/controller/chat/azure_controller.rs
+++ src/controller/chat/azure_controller.rs
@@ -1,6 +1,6 @@
 use crate::{
-    common::error::chat_error::ChatError, model::req::chat::ask_req::AskReq,
-    service::chat::azure_chat_service::azure_chat,
+    model::req::chat::ask_req::AskReq,
+    service::chat::azure_chat_service::{azure_chat, do_msg_send_sync},
 };
 use actix_web::{
     get,
@@ -9,12 +9,9 @@ use actix_web::{
 };
 use log::error;
 use rust_wheel::{
-    common::{
-        util::{
-            net::{sse_message::SSEMessage, sse_stream::SseStream},
-            time_util::get_current_millisecond,
-        },
-        wrapper::actix_http_resp::box_err_actix_rest_response,
+    common::util::{
+        net::{sse_message::SSEMessage, sse_stream::SseStream},
+        time_util::get_current_millisecond,
     },
     config::{
         app::app_conf_reader::get_app_config,
@@ -53,7 +50,16 @@ pub async fn ask(
         return handle_chat(&params.0, &login_user_info);
     }
     if chat_count.unwrap().parse::<i32>().unwrap() > 2 {
-        return box_err_actix_rest_response(ChatError::ExceedTheChatPerDayLimit);
+        let (tx, rx): (
+            UnboundedSender<SSEMessage<String>>,
+            UnboundedReceiver<SSEMessage<String>>,
+        ) = tokio::sync::mpsc::unbounded_channel();
+        do_msg_send_sync(&"vip-expire".to_string(), &tx, "chat");
+        let response = HttpResponse::Ok()
+            .insert_header(CacheControl(vec![CacheDirective::NoCache]))
+            .content_type("text/event-stream")
+            .streaming(SseStream { receiver: Some(rx) });
+        response
     } else {
         increase_chaht_count(&user_failed_key);
         return handle_chat(&params.0, &login_user_info);
@@ -83,7 +89,7 @@ fn handle_chat(req: &AskReq, login_user_info: &LoginUserInfo) -> HttpResponse {
 fn increase_chaht_count(cached_key: &String) {
     let app_str = sync_get_str(&cached_key);
     if app_str.is_none() {
-        set_str(&cached_key, "1", 120);
+        set_str(&cached_key, "1", 86400);
     } else {
         let incre_result = incre_redis_key(&cached_key, 1);
         if let Err(err) = incre_result {
